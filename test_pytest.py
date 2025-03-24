b9d8b98f65c257eb9aa2c2cec90dd8803a9bccad
Komposthaufen <164004755+Komposthaufen@users.noreply.github.com>
2025-03-24 22:23:08 +0100

    change to fit linter
    changed == to is true

--- test_pytest.py
+++ test_pytest.py
@@ -4,18 +4,18 @@ from Main import is_even
 from Main import is_odd
 
 def test_is_prime():
-    assert is_prime(3) == True
-    assert is_prime(7) == True
-    assert is_prime(4) == False
+    assert is_prime(3) is True
+    assert is_prime(7) is True
+    assert is_prime(4) is False
 
 
 def test_is_even():
-    assert is_even(3) == False
-    assert is_even(4) == True
-    assert is_even(12) == True
+    assert is_even(3) is False
+    assert is_even(4) is True
+    assert is_even(12) is True
 
 
 def test_is_odd():
-    assert is_odd(3) == True
-    assert is_odd(5) == True
-    assert is_odd(12) == False
+    assert is_odd(3) is True
+    assert is_odd(5) is True
+    assert is_odd(12) is False
